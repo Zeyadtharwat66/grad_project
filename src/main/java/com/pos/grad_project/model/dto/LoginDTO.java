7925d0c14445f8a7476951feddc8d18c1d2b0b80
Zeyadtharwat66 <ziadtharwatsoliman@gmail.com>
2026-09-19 20:58:47 +0300

    Use Jakarta validation for login DTO

--- src/main/java/com/pos/grad_project/model/dto/LoginDTO.java
+++ src/main/java/com/pos/grad_project/model/dto/LoginDTO.java
@@ -1,9 +1,9 @@
 package com.pos.grad_project.model.dto;
 
-import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 public record LoginDTO(@NotBlank(message = "username is required")
                        String username,
-                       @NotBlank(message = "username is required")
+                       @NotBlank(message = "password is required")
                        String password) {
 }
