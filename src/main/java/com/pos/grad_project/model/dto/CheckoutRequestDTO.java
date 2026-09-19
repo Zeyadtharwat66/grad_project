edcb9dfd1df57544b1bf502f4f91992d70f39e65
Zeyadtharwat66 <ziadtharwatsoliman@gmail.com>
2026-09-19 20:58:04 +0300

    Validate checkout requests

--- src/main/java/com/pos/grad_project/model/dto/CheckoutRequestDTO.java
+++ src/main/java/com/pos/grad_project/model/dto/CheckoutRequestDTO.java
@@ -1,6 +1,11 @@
 package com.pos.grad_project.model.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.List;
 
-public record CheckoutRequestDTO(List<CoursesCheckOutReqDTO> courses) {
-}
+public record CheckoutRequestDTO(
+        @NotEmpty(message = "At least one course is required")
+        List<@Valid CoursesCheckOutReqDTO> courses
+) {}
